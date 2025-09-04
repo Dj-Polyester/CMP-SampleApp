5e0ac4917a462f14138822c08d10bae5edb9b09a
Dj-Polyester <45545087+Dj-Polyester@users.noreply.github.com>
2025-09-05 02:55:38 +0900

    traverse dfs implementation

--- traverse.py
+++ traverse.py
@@ -1,149 +1,195 @@
-from typing import Any, Callable, Optional, Iterable
+from typing import Any, Callable, Optional
 from collections import deque
-from utils import exists, setattrs
+from utils import exists, setattrs, hasattrs, stringify
 from dataclasses import dataclass, field
 
 @dataclass
-class TraverseContainerArgs:
-	_type: type = list
-	top_index: Optional[int] = -1
-	pop_func: Optional[str] = "pop"
-	add_func: Optional[str] = "append"
-	add_multiple_func: Optional[Callable] = None
-
+class TraverseContainerConfig:
+	_type: type
+	top_index: int
+	pop_func: str
+	add_single_func: str = "append"
+	add_multiple_func: str = "extend"
 class TraverseContainer:
-	PRECONFIGS = {
-		"bfs": TraverseContainerArgs(deque, 0, "popleft"),
-		"dfs": TraverseContainerArgs(),
-	}
-	def __init__(
-		self,
-		traverse_type: Optional[str] = None,
-		args: Optional[TraverseContainerArgs] = None,
-	):
-		if traverse_type != None:
-			self.args = TraverseContainer.PRECONFIGS[traverse_type]
-		elif args != None:
-			self.args = args
-		else:
-			raise ValueError("Either traverse type or arguments must be provided")
+	def __init__(self, config: TraverseContainerConfig):
+		self.config = config
+	def setup(self, args=None):
+		self._container = self.config._type() if args == None else self.config._type(args)
+		self.pop = getattr(self._container, self.config.pop_func)
+		self.add_single = getattr(self._container, self.config.add_single_func)
+		self.add_multiple = getattr(self._container, self.config.add_multiple_func)
 	def __call__(self, args=None):
-		self._container = self.args._type() if args == None else self.args._type(args)
-		self.pop = getattr(self._container, self.args.pop_func)
-		self.add = getattr(self._container, self.args.add_func)
-		self.add_multiple = getattr(self._container, self.args.add_multiple_func)
-		return self
+		container = TraverseContainer(self.config)
+		container.setup(args)
+		return container
+	def empty(self):
+		return not self._container
 	def top(self):
-		return self._container[self.args.top_index] if self._container else None
+		return self._container[self.config.top_index] if self._container else None
+
+@dataclass
+class TraverseTypeConfig:
+	container: TraverseContainerConfig
+	add_special_token_before: bool
+	proc_fun: Callable
 
 class Traverse:
-	def __init__(self, _type: Optional[str] = None):
-		self.type = _type
-		self.containercls = TraverseContainer(_type)
+	PRECONFIGS = {
+		"bfs": TraverseTypeConfig(
+			container = TraverseContainerConfig(deque, 0, "popleft"),
+			add_special_token_before=False,
+			proc_fun=lambda x: x,
+		),
+		"dfs": TraverseTypeConfig(
+			container = TraverseContainerConfig(list, -1, "pop"),
+			add_special_token_before=True,
+			proc_fun=lambda x: reversed(x),
+		),
+	}
+	VALID_CALLBACKS = [
+		"callback",
+		"after",
+		"cleanup",
+	]
+	def __getattr__(self, name: str):
+		if name in Traverse.PRECONFIGS:
+			def _recurse(
+				obj: Optional[Any] = None,
+				*setup_args,
+				**setup_kwargs,
+			):
+				self.recurse(
+					obj,
+					_type=name,
+					*setup_args,
+					**setup_kwargs,
+				)
+			return _recurse
+		else:
+			raise AttributeError(f"{type(self)} object has no attribute {name}")
+	def setup(
+		self,
+		_type: Optional[str] = None,
+		config: Optional[TraverseTypeConfig] = None,
+		special_token: Any = None,
+		**callbacks: Callable,
+	):
+		# Config
+		if _type != None:
+			self.config = Traverse.PRECONFIGS[_type]
+		elif config != None:
+			self.config = config
+		elif not exists(self, "config") or not isinstance(self.config, TraverseTypeConfig):
+			raise ValueError("Either traverse type or config must be provided")
+		# Container
+		if not exists(self, "containercls"):
+			self.containercls = TraverseContainer(self.config.container)
+		# Special token
+		self.special_token = special_token
+		# Callbacks
+		self.callbacks = callbacks
+		setattrs(self.callbacks, self)
+		if not hasattrs(self, self.VALID_CALLBACKS):
+			raise AttributeError(
+				f"Object should implement {stringify(Traverse.VALID_CALLBACKS)}"
+			)
 
 	def recurse(
 		self,
-		obj: Any,
-		callback: Callable = lambda _,__: _,
-		after: Callable = lambda _, __: _,
-		cleanup: Callable = lambda _, __: _,
-		_type: str = "bfs",
+		obj: Optional[Any] = None,
+		*setup_args,
+		**setup_kwargs,
 	):
+		self.setup(*setup_args, **setup_kwargs)
+		if obj == None:
+			obj = self
 		container = self.containercls([obj])
 		parent_container = self.containercls()
 		def _neg(res):
 			return isinstance(res, bool) and not res
 		success = False
 		node = None
-		while container:
+		while not container.empty():
 			node = container.pop()
-			if node == None: # Processed all children
+			if node == self.special_token: # Processed all children
 				node = parent_container.pop()
-				success = after(parent_container.top(), node)
+				success = self.after(parent_container.top(), node)
 			else:
-				if _type == "dfs":
-					container.add(None)
+				if self.config.add_special_token_before:
+					container.add_single(self.special_token)
 				if exists(node, "children"):
 					children = node.children.values() if isinstance(node.children, dict) else node.children
-					container.extend(arg.proc_func(children))
-				if _type == "bfs":
-					container.append(None)
-				parent_node = get_parent()
-				parent_container.append(node)
-				success = callback(parent_node, node)
+					container.add_multiple(self.config.proc_fun(children))
+				if not self.config.add_special_token_before:
+					container.add_single(self.special_token)
+				parent_node = parent_container.top()
+				parent_container.add_single(node)
+				success = self.callback(parent_node, node)
 			if _neg(success):
 				break
-		while parent_container:
-			cleanup(get_parent(), node)
-			node = getattr(parent_container, arg.pop)()
-
-def bfs(
-	obj: Any,
-	callback: Callable = lambda _,__: _,
-	after: Callable = lambda _, __: _,
-	cleanup: Callable = lambda _, __: _,
-):
-	search(
-		obj,
-		callback,
-		after,
-		cleanup,
-		"bfs"
-	)
-def dfs(
-	obj: Any,
-	callback: Callable = lambda _,__: _,
-	after: Callable = lambda _, __: _,
-	cleanup: Callable = lambda _, __: _,
-):
-	search(
-		obj,
-		callback,
-		after,
-		cleanup,
-		"dfs"
-	)
+		while not parent_container.empty():
+			self.cleanup(parent_container.top(), node)
+			node = parent_container.pop()
 
 if __name__ == "__main__":
-	class Dummy:
+	def callback(parent, obj):
+		parent_id = "" if parent == None else parent.id
+		print(f"callback obj {parent_id} {obj.id}")
+	def after(parent, obj):
+		parent_id = "" if parent == None else parent.id
+		print(f"after obj {parent_id} {obj.id}")
+	def cleanup(parent, obj):
+		parent_id = "" if parent == None else parent.id
+		print(f"cleanup obj {parent_id} {obj.id}")
+
+	class DummyBase(Traverse):
 		def __init__(self, id, *children):
 			self.id = id
 			self.children = children
-	def before(obj):
-		print(f"before obj {obj.id}")
-	def after(obj):
-		print(f"after obj {obj.id}")
+	class Dummy(DummyBase):
+		def __call__(self):
+			self.bfs(
+				callback = callback,
+				after = after,
+				cleanup = cleanup,
+			)
 
-	d = Dummy(
+	class IDummy(DummyBase):
+		@staticmethod
+		def callback(parent, obj):
+			parent_id = "" if parent == None else parent.id
+			print(f"callback obj {parent_id} {obj.id}")
+		@staticmethod
+		def after(parent, obj):
+			parent_id = "" if parent == None else parent.id
+			print(f"after obj {parent_id} {obj.id}")
+		@staticmethod
+		def cleanup(parent, obj):
+			parent_id = "" if parent == None else parent.id
+			print(f"cleanup obj {parent_id} {obj.id}")
+		def __call__(self):
+			self.bfs()
+
+	cls_name = Dummy
+	print(cls_name.__name__)
+	d = cls_name(
 		1,
-		Dummy(
+		cls_name(
 			2,
-			Dummy(
+			cls_name(
 				4
 			),
-			Dummy(
+			cls_name(
 				5
 			),
 		),
-		Dummy(
+		cls_name(
 			3,
-			Dummy(
+			cls_name(
 				6
 			),
-			Dummy(
+			cls_name(
 				7
 			),
 		),
-	)
-	print("dfs")
-	dfs(
-		d,
-		before,
-		after,
-	)
-	print("\nbfs")
-	bfs(
-		d,
-		before,
-		after,
-	)
+	)()
