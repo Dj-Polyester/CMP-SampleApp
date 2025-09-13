f96d43b285220de2157ac8ef69d6980c41a77b11
Dj-Polyester <45545087+Dj-Polyester@users.noreply.github.com>
2025-09-14 00:13:23 +0300

    start reimplementation

--- traverse.py
+++ traverse.py
@@ -1,56 +1,192 @@
-from typing import Any, Callable, Optional, Union
+from typing import Any, Callable, Optional, Union, Mapping
 from collections import deque
 from dataclasses import dataclass
-from utils import exists, setattrs, hasattrs, stringify, status_msg
+from utils import (
+	exists,
+	setattrs,
+	setattrs_notnone,
+	setattrs_check,
+	stringify,
+	stringify_map,
+	status_msg,
+	Config,
+	State,
+	product_dict,
+	InvalidType,
+	InvalidValue,
+)
 from test import Test
 
-class Config: pass
+StrConfNone = Optional[Union[str, Config]]
 
-def set_config(obj: Any, config: Union[str, Config]):
-	if isinstance(config, str):
-		obj.config = type(obj).PRECONFIGS[config]
-	elif isinstance(config, Config):
-		obj.config = config
-	elif not exists(obj, "config"):
-		raise TypeError(f"Config has type {type(config)}")
+def set_configs(obj: Any, config: StrConfNone = None, **configs: StrConfNone):
+	if config != None:
+		configs["config"] = config
+	def set_config(config: StrConfNone):
+		if isinstance(config, str):
+			return type(obj).PRECONFIGS[config]
+		elif isinstance(config, Config):
+			return config
+		elif not exists(obj, "config"):
+			raise InvalidType("Config", config, (str, Config))
+	setattrs_notnone(configs, obj, callback_val = set_config)
 
-@dataclass
 class TraverseContainerConfig(Config):
-	_type: type
-	top_index: int
-	pop_func: str
-	add_single_func: str = "append"
-	add_multiple_func: str = "extend"
+	VALID_FUNCS = {
+		"pop": None,
+		"add_single": "append",
+		"add_multiple": "extend",
+	}
+	def __init__(self, **kwargs):
+		valid_funcs = {f"{k}_func": v for k, v in TraverseContainerConfig.VALID_FUNCS.items()}
+		kwargs = {**valid_funcs, **kwargs}
+		super().__init__(**kwargs)
+	def __repr__(self):
+		funcs = {
+			f"{k}_func": getattr(self, f"{k}_func") for k in TraverseContainerConfig.VALID_FUNCS.keys()
+		}
+		kwargs = {
+			"_type": self._type.__name__,
+			"top_index": self.top_index,
+			**funcs,
+		}
+		return f"TraverseContainerConfig({stringify_map(kwargs)})"
 class TraverseContainer:
+	"""
+	Arguments for the configuration parameter:
+		`_type (type)`: type of the container used such as deque or list
+		`top_index (int)`: the index used in `top` method
+
+		The following fields are name of the methods available
+		for objects of type `_type` for which corresponding method is a placeholder for.
+		`pop_func (str):` the `pop` method
+		`add_single_func (str):` the `add_single` method
+		`add_multiple_func (str):` the `add_multiple` method
+	Given a configuration, the container class implements the following methods automatically:
+		`__call__(self, items=None) -> TraverseContainer`: Adds given items to the container and
+		implements the following methods returning the container
+			`pop() -> Any`: remove and return the element
+			`add_single(item: Any)`: Add a single item to the container
+			`add_multiple(items: Collection[Any])`: Add multiple items to the container
+		`top() -> Any`: only return the element at the index configured by `top_index`
+		`empty() -> bool`: return if the container is empty
+	"""
 	PRECONFIGS = {
-		"stack": TraverseContainerConfig(list, -1, "pop"),
-		"queue": TraverseContainerConfig(deque, 0, "popleft"),
+		"stack": TraverseContainerConfig(_type=list, top_index=-1, pop_func="pop"),
+		"queue": TraverseContainerConfig(_type=deque, top_index=0, pop_func="popleft"),
 	}
 	def __init__(
 		self,
 		config: Union[str, TraverseContainerConfig],
 	):
-		set_config(self, config)
-	def setup(self, args=None):
-		self._container = self.config._type() if args == None else self.config._type(args)
-		self.pop = getattr(self._container, self.config.pop_func)
-		self.add_single = getattr(self._container, self.config.add_single_func)
-		self.add_multiple = getattr(self._container, self.config.add_multiple_func)
-	def __call__(self, args=None):
+		set_configs(self, config)
+	def setup(self, items=None):
+		self._container = self.config._type() if items == None else self.config._type(items)
+		setattrs(
+			self.config,
+			self,
+			TraverseContainerConfig.VALID_FUNCS,
+			callback_name = lambda name: f"{name}_func",
+			callback_val = lambda val: getattr(self._container, val)
+		)
+	def __call__(self, items=None):
 		container = TraverseContainer(self.config)
-		container.setup(args)
+		container.setup(items)
 		return container
 	def empty(self):
 		return not self._container
 	def top(self):
 		return self._container[self.config.top_index] if self._container else None
+	def __repr__(self):
+		return repr(self._container)
+
+class TraverseTypeConfig(Config):
+	VALID_CONTAINERS = {
+		"container": None,
+		"parent_container": None,
+		"backtrack_container": "stack",
+	}
+	def __init__(self, **kwargs):
+		kwargs = {**TraverseTypeConfig.VALID_CONTAINERS, **kwargs}
+		super().__init__(**kwargs)
+
+class TraverseStateConfig(Config):
+	def __init__(
+		self,
+		special_token: Any = None,
+		backward_mode: str = "backtrace",
+		verbose: bool = False,
+		**callbacks,
+	):
+		self.special_token = special_token
+		self.backward_mode = backward_mode
+		self.verbose = verbose
+		self.callbacks = callbacks
 
 @dataclass
-class TraverseTypeConfig(Config):
-	add_special_token_before: bool
-	proc_fun: Callable
-	container: Union[str, TraverseContainerConfig]
-	parent_container: Union[str, TraverseContainerConfig] = "stack"
+class TraverseState(State):
+	def __init__(
+		self,
+		config: Optional[TraverseStateConfig] = None,
+		type_config: Optional[Union[str, TraverseTypeConfig]] = None,
+		**containers,
+	):
+		setattrs_check(
+			containers,
+			self,
+			Traverse.VALID_CONTAINER_NAMES,
+		)
+		self.setup(config, type_config)
+	def setup(
+		self,
+		config: Optional[TraverseStateConfig] = None,
+		type_config: Optional[Union[str, TraverseTypeConfig]] = None,
+	):
+		# Configs
+		set_configs(
+			self,
+			config,
+			type_config=type_config,
+		)
+	def pop(self, container_name: str):
+		self.node = getattr(self, container_name).pop()
+	def add_single(self, container_name: str, item: Optional[Any] = None):
+		add_item = self.node if item == None else item
+		getattr(self, container_name).add_single(add_item)
+	def run(self, func_name: str, container_name_parent: str):
+		self.success = getattr(self.config, func_name)(
+			getattr(self, container_name_parent).top(),
+			self.node,
+		)
+	def add_children(self):
+		if self.type_config.add_special_token_before:
+			self.add_single("container", self.config.special_token)
+		if exists(self.node, "children"):
+			children = self.node.children
+			if isinstance(children, dict):
+				children = children.values()
+			self.container.add_multiple(self.type_config.proc_fun(children))
+		if not self.type_config.add_special_token_before:
+			self.add_single("container", self.config.special_token)
+	def forward(self):
+		self.pop("container")
+		if self.node == self.config.special_token: # Processed all children
+			self.pop("parent_container")
+			self.run("node_after", "parent_container")
+		else:
+			self.add_children()
+			self.run("node_init", "parent_container")
+			self.add_single("parent_container")
+			self.add_single("backtrack_container")
+	def backward_container(self, container_name: str):
+		self.pop(container_name)
+		self.run("node_backward", container_name)
+	def backward_parent(self):
+		self.config.node_backward(self.node.parent, self.node)
+		self.node = self.node.parent
+	def print(self):
+		if self.config.verbose:
+			print(self)
 
 class Traverse:
 	PRECONFIGS = {
@@ -65,85 +201,98 @@ class Traverse:
 			container = "stack",
 		),
 	}
-	VALID_CALLBACKS = [
-		"callback",
-		"after",
-		"cleanup",
-	]
+	VALID_CALLBACKS = (
+		"node_init",
+		"node_after",
+		"node_backward",
+	)
+	VALID_BACKWARD_MODES = (
+		"backtrace",
+		"parent",
+		"parent_dfs",
+	)
+	VALID_CONTAINER_NAMES = (
+		"container",
+		"parent_container",
+		"backtrack_container",
+	)
+	def setup(self, config: Union[str, TraverseTypeConfig]):
+		# Configs
+		set_configs(self, config)
+		# Containers
+		if not exists(self, "containercls"):
+			_c = self.config.container
+			_pc = self.config.parent_container
+			self.containercls = TraverseContainer(_c)
+			self.parent_containercls = TraverseContainer(_c) if _pc==None else TraverseContainer(_pc)
+			self.backtrack_containercls = TraverseContainer(self.config.backtrack_container)
+		# Callbacks
+		setattrs_check(
+			self.config.callbacks,
+			self,
+			Traverse.VALID_CALLBACKS,
+		)
 	def __getattr__(self, name: str):
 		if name in Traverse.PRECONFIGS:
 			def _recurse(
+				state_config: TraverseStateConfig,
 				obj: Optional[Any] = None,
-				*setup_args,
-				**setup_kwargs,
 			):
 				self.recurse(
-					obj,
-					config=name,
-					*setup_args,
-					**setup_kwargs,
+					state_config=state_config,
+					type_config=name,
+					obj=obj,
 				)
 			return _recurse
+		elif exists(self, "state") and exists(self.state, name):
+			return getattr(self.state, name)
 		else:
 			raise AttributeError(f"{type(self)} object has no attribute {name}")
-	def setup(
-		self,
-		config: Union[str, TraverseTypeConfig],
-		special_token: Any = None,
-		**callbacks: Callable,
-	):
-		# Config
-		set_config(self, config)
-		# Containers
-		if not exists(self, "containercls"):
-			self.containercls = TraverseContainer(self.config.container)
-			self.parent_containercls = TraverseContainer(self.config.parent_container)
-		# Special token
-		self.special_token = special_token
-		# Callbacks
-		self.callbacks = callbacks
-		setattrs(self.callbacks, self)
-		if not hasattrs(self, self.VALID_CALLBACKS):
-			raise AttributeError(
-				f"Object should implement {stringify(Traverse.VALID_CALLBACKS)}"
-			)
-
+	def print(self, *args, **kwargs):
+		if exists(self, "verbose") and self.verbose:
+			print(*args, **kwargs)
 	def recurse(
 		self,
+		state_config: TraverseStateConfig,
+		type_config: Union[str, TraverseTypeConfig],
 		obj: Optional[Any] = None,
-		*setup_args,
-		**setup_kwargs,
 	):
-		self.setup(*setup_args, **setup_kwargs)
+		self.setup(type_config)
 		if obj == None:
 			obj = self
-		container = self.containercls([obj])
-		parent_container = self.containercls()
-		def _neg(res):
-			return isinstance(res, bool) and not res
-		success = False
-		node = None
-		while not container.empty():
-			node = container.pop()
-			if node == self.special_token: # Processed all children
-				node = parent_container.pop()
-				success = self.after(parent_container.top(), node)
-			else:
-				if self.config.add_special_token_before:
-					container.add_single(self.special_token)
-				if exists(node, "children"):
-					children = node.children.values() if isinstance(node.children, dict) else node.children
-					container.add_multiple(self.config.proc_fun(children))
-				if not self.config.add_special_token_before:
-					container.add_single(self.special_token)
-				parent_node = parent_container.top()
-				parent_container.add_single(node)
-				success = self.callback(parent_node, node)
-			if _neg(success):
-				break
-		while not parent_container.empty():
-			self.cleanup(parent_container.top(), node)
-			node = parent_container.pop()
+		self.state = TraverseState(
+			container = self.containercls([obj]),
+			parent_container = self.parent_containercls(),
+			backtrack_container = self.backtrack_containercls(),
+			config = state_config,
+			type_config = type_config,
+		)
+		self.state.print()
+		while not self.state.container.empty():
+			self.forward()
+			self.state.print()
+			if self.state.failed(): break
+		return self.state
+	def recurse_backward(self):
+		self.state.print()
+		if self.state.config.backward_mode == "backtrace":
+			while not self.state.backtrack_container.empty():
+				self.state.backward_container("backtrack_container")
+				self.state.print()
+		elif self.state.config.backward_mode == "parent":
+			while exists(self.state.node, "parent"):
+				self.state.backward_parent()
+				self.state.print()
+		elif self.state.config.backward_mode == "parent_dfs":
+			while not self.state.backtrack_container.empty():
+				self.state.backward_container("parent_container")
+				self.state.print()
+		else:
+			raise InvalidValue(
+				"Backward mode",
+				self.state.config.backward_mode,
+				Traverse.VALID_BACKWARD_MODES,
+			)
 
 if __name__ == "__main__":
 	def callbacktxt(prefix, parent, obj):
@@ -151,78 +300,123 @@ if __name__ == "__main__":
 		_callbacktxt = f"{prefix} {parent_id} {obj.id}"
 		DummyBase.buffertxt.append(_callbacktxt)
 		print(_callbacktxt)
-	def callback(*args):
-		callbacktxt("callback", *args)
-	def after(*args):
-		callbacktxt("after", *args)
-	def cleanup(*args):
-		callbacktxt("cleanup", *args)
+	def node_init(*args):
+		callbacktxt("node_init", *args)
+		if args[1].id == 5:
+			return False
+	def node_after(*args):
+		callbacktxt("node_after", *args)
+	def node_backward(*args):
+		callbacktxt("node_backward", *args)
 
 	class DummyBase(Traverse):
 		buffertxt: list
 		def __init__(self, id, *children):
 			DummyBase.buffertxt = []
 			self.id = id
+			self.set_children(children)
+		def set_children(self, children):
 			self.children = children
+			for child in self.children:
+				child.parent = self
+		def __repr__(self):
+			return str(self.id)
 	class Dummy(DummyBase):
-		def __call__(self, _type):
+		def __call__(self, _type, state_config):
 			getattr(self, _type)(
-				callback = callback,
-				after = after,
-				cleanup = cleanup,
+				state_config = state_config
 			)
 
 	class IDummy(DummyBase):
 		@staticmethod
-		def callback(*args):
-			callbacktxt("callback", *args)
+		def node_init(*args):
+			return node_init(*args)
 		@staticmethod
-		def after(*args):
-			callbacktxt("after", *args)
+		def node_after(*args):
+			return node_after(*args)
 		@staticmethod
-		def cleanup(*args):
-			callbacktxt("cleanup", *args)
-		def __call__(self, _type):
-			getattr(self, _type)()
+		def node_backward(*args):
+			return node_backward(*args)
+		def __call__(self, _type, backward_mode):
+			getattr(self, _type)(backward_mode = backward_mode)
 
 	class TraverseTest(Test):
+		def containers(self):
+			params = {
+				"container_config" : [
+					"stack",
+					"queue",
+					TraverseContainerConfig(_type=deque, top_index=0, pop_func="popleft"),
+				],
+				"items": [
+					[31, 69],
+					[],
+				]
+			}
+
+			for i, config in enumerate(product_dict(params), 1):
+				container_cls = TraverseContainer(config["container_config"])
+				container = container_cls(config["items"])
+				print(f"Container {i}")
+				print(f"Config: {config}")
+				print(container)
+				if container.empty():
+					print("Container is empty")
+				else:
+					print(
+f"""Container is not empty
+Top value at index {container.config.top_index} is {container.top()}
+Container after popping {container.pop()}: {container}
+"""					)
+				_item = 42
+				container.add_single(_item)
+				print(f"Container after adding {_item}: {container}")
+				_items = [29, 86]
+				container.add_multiple(_items)
+				print(f"Container after adding {_items}: {container}\n")
+
+
 		def compare_implementations(self):
 			buffer_txts = []
-			_types = Traverse.PRECONFIGS
-			cls_names = [
+			params = {
+				"_type": Traverse.PRECONFIGS.keys(),
+				"backward_mode": Traverse.VALID_BACKWARD_MODES,
+			}
+			cls_types = [
 				Dummy,
 				IDummy,
 			]
-			cls_names_str = [t.__name__ for t in cls_names]
-			for _type in _types:
-				for cls_name, cls_name_str in zip(cls_names, cls_names_str):
-					print(f"\n{cls_name_str} {_type}")
-					cls_name(
+			cls_names = [t.__name__ for t in cls_types]
+			for param in product_dict(params):
+				print(f"Parameters: {param}")
+				for cls_type, cls_name in zip(cls_types, cls_names):
+					print(f"Class name: {cls_name}")
+					cls_type(
 						1,
-						cls_name(
+						cls_type(
 							2,
-							cls_name(
+							cls_type(
 								4
 							),
-							cls_name(
+							cls_type(
 								5
 							),
 						),
-						cls_name(
+						cls_type(
 							3,
-							cls_name(
+							cls_type(
 								6
 							),
-							cls_name(
+							cls_type(
 								7
 							),
 						),
-					)(_type)
+					)(**param)
 					buffer_txts.append(DummyBase.buffertxt)
 				is_equal = buffer_txts[0] == buffer_txts[1]
 				print(
-					f"\n{stringify(cls_names_str)} {_type} {status_msg(int(not is_equal))}"
+					f"\n{stringify(cls_names)} {status_msg(int(not is_equal))}"
 				)
 				assert is_equal
 	test=TraverseTest()
-	test()
+	test.containers()
--- utils.py
+++ utils.py
@@ -1,4 +1,149 @@
-from typing import Any, Iterable, Optional, Collection, Sequence
+from typing import (
+	Union,
+	Any,
+	Iterable,
+	Optional,
+	Collection,
+	Sequence,
+	Mapping,
+	MutableMapping,
+	Callable,
+	Container,
+)
+import itertools
+from dataclasses import dataclass
+
+class InvalidType(TypeError):
+    """Raised when a variable has an invalid type"""
+    def __init__(self, varname: str, var, items: Sequence):
+        super().__init__(
+       		f"The {varname} has to be of type {stringify(items, 'or')} "
+        	f"but has type {type(var)}"
+        )
+class InvalidValue(ValueError):
+    """Raised when a variable has an invalid type"""
+    def __init__(self, varname: str, var, items: Sequence):
+        super().__init__(
+       		f"The {varname} has to be {stringify(items, 'or')} "
+        	f"but is {var}"
+        )
+
+
+@dataclass
+class State:
+	success: Optional[bool]
+	def completed(self):
+		return not isneg(self.success)
+	def failed(self):
+		return isneg(self.success)
+class Config:
+	def __init__(self, **kwargs):
+		setattrs(kwargs, self)
+
+def product_dict(
+	valid_mapping: Mapping,
+	condition: Callable = lambda **_: True,
+
+):
+	"""
+	Given a `valid_mapping` of key value pairs as (name, iterable_of_valid_values),
+	generates a mapping that satisfies `condition` for each iteration as
+	(name, possible_value) using cartesian product.
+
+	Usage:
+```
+for combination in product_dict(valid_mapping, condition):
+	# Do sth with the combination
+	...
+```
+	"""
+	keys = valid_mapping.keys()
+	iter_of_valid_vals = valid_mapping.values()
+	for val_comb in itertools.product(*iter_of_valid_vals):
+		comb = dict(zip(keys, val_comb))
+		if condition(**comb):
+			yield comb
+
+def setattrs_check(
+	src: Union[Any, Collection],
+	dst: Any,
+	iterable: Sequence,
+	callback: Callable = lambda x: x,
+	condition: Callable = lambda _: True,
+):
+	setattrs(
+		src,
+		dst,
+		callback=callback,
+		condition=condition,
+	)
+	if not hasattrs_all(dst, iterable):
+		raise AttributeError(
+			f"Object should implement or provide {stringify(iterable)} as parameters"
+		)
+
+def setattrs_notnone(
+	src: Union[Any, Collection],
+	dst: Any,
+	iterable: Optional[Iterable[str]] = None,
+	callback_name: Callable = lambda x: x,
+	callback_val: Callable = lambda x: x,
+):
+	setattrs(
+		src,
+		dst,
+		iterable,
+		callback_name,
+		callback_val,
+		lambda x: x != None,
+	)
+
+def setattrs(
+	src: Union[Any, Collection],
+	dst: Any,
+	iterable: Optional[Iterable[str]] = None,
+	callback_name: Callable = lambda x: x,
+	callback_val: Callable = lambda x: x,
+	condition: Callable = lambda _: True,
+):
+	"""
+	Copy attributes of src to dst. If iterable of attribute names is
+	provided, it is iterated upon for the attributes.
+	Otherwise, the src has to inherit from 'Collection'
+	"""
+	def getfunc(obj: Any):
+		return obj.__getitem__ if isinstance(obj, MutableMapping) else obj.__getattribute__
+	def setfunc(obj: Any):
+		return obj.__setitem__ if isinstance(obj, MutableMapping) else obj.__setattr__
+	if iterable == None:
+		if isinstance(src, Collection):
+			iterable = src
+		else:
+			raise TypeError(f"src parameter of type {type(src)} should be 'Collection'")
+	src_getfunc = getfunc(src)
+	dst_setfunc = setfunc(dst)
+	for name in iterable:
+		src_val = src_getfunc(callback_name(name))
+		if condition(src_val):
+			dst_setfunc(name, callback_val(src_val))
+
+def hasattrs_all(obj: Union[Any, Container], attrs: Iterable):
+	for attr in attrs:
+		if isinstance(obj, Container):
+			if attr not in obj:
+				return False
+		elif not exists(obj, attr):
+			return False
+	return True
+
+def hasattrs_any(obj: Union[Any, Container], attrs: Iterable):
+	for attr in attrs:
+		if isinstance(obj, Container):
+			if attr in obj:
+				return True
+		elif exists(obj, attr):
+			return True
+	return False
 
 STATUS = [
 	"✓",
@@ -12,31 +157,14 @@ def status_msg(code: int):
 	except IndexError:
 		raise ValueError(f"Invalid status {code}")
 
-def setattrs(src: Collection, dst: Any, iterable: Optional[Iterable] = None):
-	def getfunc(obj: Any):
-		return obj.__getitem__ if isinstance(obj, dict) else obj.__getattribute__
-	def setfunc(obj: Any):
-		return obj.__setitem__ if isinstance(obj, dict) else obj.__setattr__
-	if iterable == None:
-		iterable = src
-	src_getfunc = getfunc(src)
-	dst_setfunc = setfunc(dst)
-	for name in iterable:
-		dst_setfunc(name, src_getfunc(name))
-def hasattrs(obj: Any, attrs: Iterable):
-	for attr in attrs:
-		if not exists(obj, attr):
-			return False
-	return True
-
-def stringify(items: Sequence, and_or = "and"):
+def stringify_map(map: Mapping):
+	return stringify([f"{k}={v}" for k, v in map.items()])
+def stringify(items: Sequence, and_or: str = ", "):
 	return ", ".join(items[:-1]) + f" {and_or} " + items[-1]
-
 def getattr_none(obj: Any, name: str):
-	return getattr(obj, name) if hasattrs(obj, name) else None
-
-def _not_implemented(*args, **kwargs):
-    raise NotImplementedError()
+	return getattr(obj, name) if hasattrs_all(obj, name) else None
+def isneg(res):
+	return isinstance(res, bool) and not res
 def exists(obj: Any, attr: str) -> bool:
 	return hasattr(obj, attr) and getattr(obj, attr) != None
 def tabbed_print(depth: int, *args, **kwargs):
