d207252f5a661b4e846fc4da68723785446060eb
Dj-Polyester <45545087+Dj-Polyester@users.noreply.github.com>
2025-10-10 19:35:04 +0300

    mv the folder to a submodule

--- android_debug.py
+++ android_debug.py
@@ -1,14 +1,27 @@
 import argparse, os
 from pathlib import Path
+import sys
 from jproperties import Properties
-from pipeline import *
+from typing import Callable
+
+from tasks import log, Task, Pipeline, Multiplexer
+from tasks.utils import multitype, _bool
+
+VALID_VERBOSE_VALUES = [
+	"VERBOSE",
+	"SILENT",
+	"DEBUG",
+	"INFO",
+	"WARNING",
+	"ERROR",
+	"CRITICAL",
+]
 
 class FileActor:
 	@classmethod
 	def ask(cls, prompt: str):
-		if args.verbose:
-			print(f"Contents of {cls.file_path}:")
-			print(cls.f.read())
+		log.print(f"Contents of {cls.file_path}:")
+		log.print(cls.f.read())
 		res: str=input(f"{prompt} (y/N)?: ")
 		return res.lower()
 	@classmethod
@@ -68,14 +81,12 @@ class PropertyActor(FileActor):
 		return property in cls.p and cls.p[property].data == target
 
 # Preprocessing utils
-def touch(file_path: Path) -> bool:
+def touch(file_path: Path):
 	file_path.touch()
 # Other utils
-def runProcess(cmd: str, **kwargs):
-	if args.verbose:
-		print(cmd)
-	cmdList = cmd.split()
-	Task.shell(cmdList, **kwargs)
+def runProcess(cmd: str, use_mp = False):
+	log.print(cmd)
+	Task(cmd, use_mp=use_mp)()
 def install(file_path: Path):
 	wiredFlag = None
 	wiredMsg = None
@@ -90,107 +101,120 @@ def install(file_path: Path):
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(
-		prog='android_debug',
-		description='Builds, installs and runs APK file',
+		prog="android_debug",
+		description="Builds, installs and runs APK file",
 	)
 	parser.add_argument(
-		'--package_name',
+		"--package_name",
 		type=str,
-		default="org.custom.slide",
-		help="""
-Package name that includes the entry point (i.e. MainActivity.kt)
-of the application when debugging via adb. This is typically the
-first package name chosen before creating the project directory.
-	""")
+		default="org.example.project",
+		metavar="(str, default: org.custom.slide)",
+		help=
+"Package name that includes the entry point (i.e. MainActivity.kt)"
+"of the application when debugging via adb. This is typically the"
+"first package name chosen before creating the project directory."
+	)
 	parser.add_argument(
-		'--log_tag',
+		"--log_tag",
 		type=str,
+		metavar="(str, default=ComposeApp)",
 		default="ComposeApp",
-		help="""
-The tag used by the Log instances. Logcat will only print the logs
-with this tag. Default is "ComposeApp"
-	""")
+		help=
+"The tag used by the Log instances. Logcat will only print the logs with this tag."
+	)
 	parser.add_argument(
-		'--subproject',
+		"--subproject",
 		type=str,
+		metavar="(str, default=composeApp)",
 		default="composeApp",
-		help="""
-Name used for the build directory, where the APK files are stored.
-Default is "composeApp". The build directory is
-[subproject]/build/outputs/apk/debug/[subproject]-debug.apk.
-	""")
+		help=
+"Name used for the build directory, where the APK files are stored."
+"The build directory is [subproject]/build/outputs/apk/debug/[subproject]-debug.apk."
+	)
 	parser.add_argument(
-		'-w',
-		'--wired',
-		type=int,
-		default=1,
-		help="""
-Whether to use wired connection or not.
-Value of 1 will attempt to debug the device connected via USB
-whereas 0 will try either the device connected wirelessly or an
-emulator running. Debugging multiple devices is not possible.
-Default is 1.
-	""")
+		"-w",
+		"--wired",
+		type=_bool,
+		metavar="(bool, default=True)",
+		default=True,
+		help=
+"Whether to use wired connection or not."
+"Value of 1 will attempt to debug the device connected via USB "
+"whereas 0 will try either the device connected wirelessly or an"
+"emulator running. Debugging multiple devices is not possible."
+	)
 	parser.add_argument(
-		'-v',
-		'--verbose',
-		type=int,
-		default=0,
-		help="""
-Prints the commands run.
-	""")
+		"-p",
+		"--preprocess",
+		type=_bool,
+		metavar="(bool, default=False)",
+		default=False,
+		help=
+"Prompts the user for series of manual handling to"
+"remove warnings/errors. "
+"In some cases, the program may automatically handle its prompts."
+	)
 	parser.add_argument(
-		'-p',
-		'--preprocess',
-		type=int,
-		default=0,
-		help="""
-Urges the user for a number of preprocessing steps to
-remove warnings/errors. Default is 0.
-	""")
+		"-a",
+		"--assemble",
+		type=_bool,
+		metavar="(bool, default=True)",
+		default=True,
+		help=
+"Whether to assemble the project and generate APK files."
+	)
 	parser.add_argument(
-		'-a',
-		'--assemble',
-		type=int,
-		default=1,
-		help="""
-Whether to assemble the project and generate APK files. Default is 1.
-	""")
+		"-i",
+		"--install",
+		type=_bool,
+		metavar="(bool, default=True)",
+		default=True,
+		help=
+"Whether to install the APK."
+	)
 	parser.add_argument(
-		'-i',
-		'--install',
-		type=int,
-		default=1,
-		help="""
-Whether to install the APK. Default is 1.
-	""")
+		"-r",
+		"--run",
+		type=_bool,
+		metavar="(bool, default=True)",
+		default=True,
+		help=
+"Whether to run the installed APK. The logs will be printed."
+	)
 	parser.add_argument(
-		'-r',
-		'--run',
-		type=int,
-		default=1,
-		help="""
-Whether to run the installed APK. The logs will be printed. Default is 1.
-	""")
-
+		"-v",
+		"--verbose",
+		type=multitype(int, str),
+		metavar="(int or str, default=warning)",
+		default="warning",
+		help=
+"Choose a verbose level, either integer or string."
+"Each given level corresponds to a logging level used in standard libraries"
+"and integer: string values are as follows:"
+"\n1: debug,"
+"\n2: info,"
+"\n3: warning,"
+"\n4: error,"
+"\n5: critical,"
+"for string values, 'verbose' and 'silent' values are also available."
+	)
 	args = parser.parse_args()
 
-
-	for key, value in vars(args).items():
-		if isinstance(value, int):
-			setattr(args, key, bool(value))
-	if args.verbose:
-		print(f"Arguments:\n{args}")
+	verbose_lwl: int = 0
+	if isinstance(args.verbose, int):
+		verbose_lwl = args.verbose * 10
+	elif args.verbose.upper() in VALID_VERBOSE_VALUES:
+		verbose_lwl = getattr(log, args.verbose.upper())
+	log.setLevel(verbose_lwl)
+	print(f"Log level {log.getEffectiveLevel()}")
+	log.print(f"Arguments:\n{args}")
 
 	subproject: str = args.subproject
 	android_home = os.environ.get("ANDROID_HOME")
 	android_debug = Pipeline(
-		"android_debug",
 		Multiplexer(
-			"preprocess",
 			args.preprocess,
 			Pipeline(
-				"preprocess",
 				#Change sdk.dir in local.properties to Android SDK location to suppress the warning
 				Task(
 					PropertyActor.act_prompted,
@@ -211,25 +235,26 @@ Whether to run the installed APK. The logs will be printed. Default is 1.
 				# touch updates the timestamp of the file resolving the issue.
 				Task(
 					touch,
-					file_path=Path(
+					Path(
 						subproject, "src", "commonMain", "composeResources", "drawable"
 					),
 				),
+				id="preprocess",
 			),
+			id="preprocess",
 		),
 		Multiplexer(
-			"assemble",
 			args.assemble,
 			Task(
 				runProcess,
 				f"./gradlew :{subproject}:assembleDebug",
+				use_mp=True,
 			),
+			id="assemble",
 		),
 		Multiplexer(
-			"install",
 			args.install,
 			Pipeline(
-				"install",
 				Task(
 					runProcess,
 					"adb devices",
@@ -238,13 +263,13 @@ Whether to run the installed APK. The logs will be printed. Default is 1.
 					install,
 					Path(subproject, "build", "outputs", "apk", "debug", f"{subproject}-debug.apk"),
 				),
+				id="install",
 			),
+			id="install",
 		),
 		Multiplexer(
-			"run",
 			args.run,
 			Pipeline(
-				"run",
 				Task(
 					runProcess,
 					f"adb shell am start -a android.intent.action.MAIN -n {args.package_name}/.MainActivity",
@@ -253,8 +278,11 @@ Whether to run the installed APK. The logs will be printed. Default is 1.
 					runProcess,
 					f"adb logcat {args.log_tag}:D *:S",
 				),
+				id="run",
 			),
+			id="run",
 		),
-		verbose = args.verbose,
+		verbose = log.level(log.VERBOSE),
+		id="android_debug",
 	)
 	android_debug()
